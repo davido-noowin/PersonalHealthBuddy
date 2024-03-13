b647a574b1efda9bb2f74120082a46a155446eac
David Nguyen <davido.nguyen1212@gmail.com>
2024-03-13 15:38:20 -0700

    setup all post functions for logging data

--- src/pages/log_data.js
+++ src/pages/log_data.js
@@ -4,6 +4,109 @@ import { Pedometer } from 'expo-sensors';
 
 import { PHB_COLORS, PHB_FONTS, PHB_STYLES } from '../phb_styles';
 import { CheckListRow, InfoContainer, PHB_Body } from '../phb_components'
+import { AuthContext } from '../../authContext';
+import React, { useContext } from 'react';
+import { getCurrentDate } from '../../current_date';
+
+
+function logFood(data, username, date) {
+    console.log("LOGGING FOOD WITH:", data, username, date);
+    /* data contains the following: 
+    *   fruits : bool
+    *   vegetables : bool
+    *   proteins : bool
+    *   grains : bool
+    *   dairy : bool
+    *  CHANGE THE DATA WITH WHATEVER THE USER INPUTS
+    */
+   const food_data_to_send = {
+        username: username,
+        date: date,
+        fruits: data.fruits,
+        vegetables: data.vegetables,
+        protein: data.protein,
+        grains: data.grains,
+        dairy: data.dairy
+   };
+
+   fetch("http://18.226.94.38:8000/api/log-food", {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(food_data_to_send)
+   })
+   .then((response) => response.json())
+   .then((responseData) => {
+    console.log(JSON.stringify(responseData));
+   })
+   .catch((error) => console.error('Error:', error));
+}
+
+
+function logExercise(data, username, date) {
+    console.log("LOGGING EXERCISE WITH:", data, username, date);
+    /* data contains the following: 
+    *   duration : int
+    *   type : {cardio, strength, yogo, pilates, sport}
+    *   steps : int - CALLED USING PEDOMETER
+    *  CHANGE THE DATA WITH WHATEVER THE USER INPUTS
+    */
+    const exercise_data_to_send = {
+        username: username,
+        date: date,
+        duration: data.duration,
+        type: data.type,
+        steps: data.steps
+   };
+
+   fetch("http://18.226.94.38:8000/api/log-exercise", {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(exercise_data_to_send)
+   })
+   .then((response) => response.json())
+   .then((responseData) => {
+    console.log(JSON.stringify(responseData));
+   })
+   .catch((error) => console.error('Error:', error));
+}
+
+
+function logWellness(data, username, date) {
+    console.log("LOGGING WELLNESS WITH:", data, username, date);
+    /* data contains the following: 
+    *   screen_duration : float
+    *   sleep_duration : float
+    *  CHANGE THE DATA WITH WHATEVER THE USER INPUTS
+    */
+    const wellness_data_to_send = {
+        username: username,
+        date: date,
+        duration: data.duration,
+        type: data.type,
+        steps: data.steps
+   };
+
+   fetch("http://18.226.94.38:8000/api/log-wellness", {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(wellness_data_to_send)
+   })
+   .then((response) => response.json())
+   .then((responseData) => {
+    console.log(JSON.stringify(responseData));
+   })
+   .catch((error) => console.error('Error:', error));
+}
+
 
 async function getStepCount(username) {
     var step_count = 0;
@@ -24,6 +127,10 @@ async function getStepCount(username) {
 };
 
 export function LogDataPage({ navigation }) {
+    const { currentUser } = useContext(AuthContext);
+
+    // logFood({"fruits" : 0, "vegetables" : 0, "protein" : 0, "grains" : 0, "dairy" : 0}, currentUser, getCurrentDate());
+
     return (
         <View style={PHB_STYLES.root_container}>
 
