0448bef00a9c638e65cd1d3c9ee96a1b21bc56aa
Ubuntu <ubuntu@ip-172-31-5-96.us-east-2.compute.internal>
2024-03-13 05:17:35 +0000

    clearing pedometer from main.py

--- src/backend/database_connection.py
+++ src/backend/database_connection.py
@@ -3,7 +3,7 @@ import mysql.connector
 # database connection removed from other python files so you can call it
 datasource = mysql.connector.connect(
     host="localhost",
-    user="root",
-    password="password",
+    user="mytestuser",
+    password="My6$Password",
     database="PHB"
 )
