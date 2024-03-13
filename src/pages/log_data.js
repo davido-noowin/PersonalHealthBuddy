44f4e1f044d9a374e32a114a97811dd88103ff3b
Jonathan Chen <jonac19@uci.edu>
2024-03-12 20:05:33 -0700

    added function to retrieve step count for iOS

--- src/pages/log_data.js
+++ src/pages/log_data.js
@@ -4,6 +4,24 @@ import { StyleSheet, Text, View, Button, ScrollView } from 'react-native';
 import { PHB_COLORS, PHB_FONTS, PHB_STYLES } from '../phb_styles';
 import { CheckListRow, InfoContainer, PHB_Body } from '../phb_components'
 
+async function getStepCount(username) {
+    var step_count = 0;
+
+    if (Platform.OS === 'ios') {
+        const isAvailable = await Pedometer.isAvailableAsync();
+
+        if (isAvailable) {
+            const start = new Date();
+            const end = new Date();
+            start.setDate(end.getDate() - 1);
+
+            step_count = await Pedometer.getStepCountAsync(start, end).steps;
+        }
+    }
+    
+    return step_count;
+};
+
 export function LogDataPage({ navigation }) {
     return (
         <View style={PHB_STYLES.root_container}>
