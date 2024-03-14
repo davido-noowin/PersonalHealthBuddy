6d9e93d12d1d01d59d26691b7b77f891cf06e1c1
Charlie Zhao <58120654+zcharz@users.noreply.github.com>
2024-03-13 19:35:45 -0700

    Update log_data.js

--- src/pages/log_data.js
+++ src/pages/log_data.js
@@ -1,14 +1,116 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, ScrollView, Switch, Platform} from 'react-native';
+import { StyleSheet, Text, View, Button, ScrollView, Switch, Platform, TextInput } from 'react-native';
 import { Pedometer } from 'expo-sensors';
 
 import { PHB_COLORS, PHB_FONTS, PHB_STYLES } from '../phb_styles';
 import { CheckListRow, InfoContainer, PHB_Body } from '../phb_components'
-
-import React, {useState, useContext} from 'react';
 import { AuthContext } from '../../authContext';
+import React, { useContext, useState } from 'react';
+import { getCurrentDate } from '../../current_date';
+import { set } from 'react-hook-form';
+// import { TextInput } from 'react-native-gesture-handler';
 
-async function getStepCount() {
+
+function logFood(data, username, date) {
+    console.log("LOGGING FOOD WITH:", data, username, date);
+    /* data contains the following: 
+    *   fruits : bool
+    *   vegetables : bool
+    *   proteins : bool
+    *   grains : bool
+    *   dairy : bool
+    *  CHANGE THE DATA WITH WHATEVER THE USER INPUTS
+    */
+   const food_data_to_send = {
+        username: username,
+        date: date,
+        fruits: data.fruits,
+        vegetables: data.vegetables,
+        protein: data.protein,
+        grains: data.grains,
+        dairy: data.dairy
+   };
+
+   fetch("http://18.226.94.38:8000/api/log-food", {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(food_data_to_send)
+   })
+   .then((response) => response.json())
+   .then((responseData) => {
+    console.log(JSON.stringify(responseData));
+   })
+   .catch((error) => console.error('Error:', error));
+}
+
+
+function logExercise(data, username, date) {
+    console.log("LOGGING EXERCISE WITH:", data, username, date);
+    /* data contains the following: 
+    *   duration : int
+    *   type : {cardio, strength, yogo, pilates, sport}
+    *   steps : int - CALLED USING PEDOMETER
+    *  CHANGE THE DATA WITH WHATEVER THE USER INPUTS
+    */
+    const exercise_data_to_send = {
+        username: username,
+        date: date,
+        duration: data.duration,
+        type: data.type,
+        steps: data.steps
+   };
+
+   fetch("http://18.226.94.38:8000/api/log-exercise", {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(exercise_data_to_send)
+   })
+   .then((response) => response.json())
+   .then((responseData) => {
+    console.log(JSON.stringify(responseData));
+   })
+   .catch((error) => console.error('Error:', error));
+}
+
+
+function logWellness(data, username, date) {
+    console.log("LOGGING WELLNESS WITH:", data, username, date);
+    /* data contains the following: 
+    *   screen_duration : float
+    *   sleep_duration : float
+    *  CHANGE THE DATA WITH WHATEVER THE USER INPUTS
+    */
+    const wellness_data_to_send = {
+        username: username,
+        date: date,
+        duration: data.duration,
+        type: data.type,
+        steps: data.steps
+   };
+
+   fetch("http://18.226.94.38:8000/api/log-wellness", {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(wellness_data_to_send)
+   })
+   .then((response) => response.json())
+   .then((responseData) => {
+    console.log(JSON.stringify(responseData));
+   })
+   .catch((error) => console.error('Error:', error));
+}
+
+
+async function getStepCount(username) {
     var step_count = 0;
 
     if (Platform.OS === 'ios') {
@@ -22,7 +124,6 @@ async function getStepCount() {
             step_count = await Pedometer.getStepCountAsync(start, end).steps;
         }
     }
-
     else {
         step_count = 1000;
     }
@@ -31,141 +132,173 @@ async function getStepCount() {
 };
 
 
-function log_food(data, setUser) {
-    console.log("SUBMITTED");
-    console.log(data);
-
-    // api call to login, set to correct address to make login work
-    fetch("http://18.226.94.38:8000/api/log-food", {
-        method: "POST",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data)
-    })
-    .then((response) => response.json())
-    .then((responseData) => {
-        console.log(JSON.stringify(responseData)); // logs the server response which should be 200 if successful
-        if (responseData.success === true) {
-            console.log("correct login info, user=" + responseData.username);
-            setUser(responseData.username)
-        }
-        else {
-            console.log("wrong login");
-            // let the user know that they put in the wrong credentials
-        }
-    })
-    .catch((error) => console.error('Error:', error));
-}
-
-
-
-
 export function LogDataPage({ navigation }) {
-    const step_count = getStepCount();
+    const step_count = getStepCount()['_j'];
     const {currentUser, setCurrentUser} = useContext(AuthContext);
-
     const [fruits, setFruit] = useState(false);
     const [vegetables, setVeg] = useState(false);
     const [grains, setGrain] = useState(false);
     const [dairy, setDairy] = useState(false);
     const [meats, setMeat] = useState(false);
-
-    const [exercise, setExercise] = useState('');
+    
+    const [exercise_duration, setExercise] = useState('');
+    const [exercise_type, setType] = useState('');
     const [sleep, setSleep] = useState('');
     const [screen_time, setScreen] = useState('');
 
-    const changeText = (setValue) = (text) => {
-        // Regular expression to allow only digits and backspace
-        const regex = /^\d+\b/;
-        const newValue = text.replace(regex, '');
-        setValue(newValue);
+    const changeExercise = (text) => {
+        const newText = text.replace(/[^0-9]/g, ''); // Filter out non-numeric characters
+        setExercise(newText);
       };
+    const changeType = (text) => { setType(text); };
+
+    const toggleFruits = () => setFruit(previousState => !previousState);
+    const toggleVeg = () => setVeg(previousState => !previousState);
+    const toggleGrain = () => setGrain(previousState => !previousState);
+    const toggleDairy = () => setDairy(previousState => !previousState);
+    const toggleMeat = () => setMeat(previousState => !previousState);
+
+    const foodPress = () => {
+        logFood({
+            fruits: fruits ? 1 : 0,
+            vegetables: vegetables ? 1 : 0,
+            protein: meats ? 1 : 0,
+            grains: grains ? 1 : 0,
+            dairy: dairy ? 1 : 0,
+        }, currentUser, getCurrentDate())
+    }
     
-    const toggleSwitch = (setIsEnabled) => setIsEnabled(previousState => !previousState);
-    console.log()
+    const exercisePress = () => {
+        logExercise({
+            duration : exercise_duration, 
+            type : exercise_type, 
+            steps : step_count
+            }, currentUser, getCurrentDate())
+    }
+
+    const wellnessPress = () => {
+        logWellness({
+            screen_duration : 0, 
+            sleep_duration : 0
+            }, currentUser, getCurrentDate())
+    }
 
     return (
         <View style={PHB_STYLES.root_container}>
             <PHB_Body scroll={true}>
             <InfoContainer title="Food Checklist">
                 <ScrollView  style={styles.scroll_view}>
-                    {/* <Text>
-                        {fruits && 'Fruits: '}
-                        {vegetables && 'Vegetables: '}
-                        {grains && 'Grains: '}
-                        {dairy && 'Dairy: '}
-                        {meats && 'Meats: '}
-                    </Text> */}
+                    <Button title='Log Food' onPress={foodPress}></Button>
 
-                    {/* <Text>{currentUser} : {step_count} steps</Text> */}
-            {/* 
-                    <Switch
-                        trackColor={{false: PHB_COLORS.WHITE, true: PHB_COLORS.SLATE}}
-                        thumbColor={fruits ? PHB_COLORS.BLUE : PHB_COLORS.LIGHTBLUE}
-                        onValueChange={toggleSwitch(setFruit)}
-                        value={fruits}
-                    />
-                    <Switch
-                        trackColor={{false: '#767577', true: '#81b0ff'}}
-                        thumbColor={vegetables ? '#f5dd4b' : '#f4f3f4'}
-                        onValueChange={toggleSwitch(setVeg)}
-                        value={vegetables}
-                    />
-                    <Switch
-                        trackColor={{false: '#767577', true: '#81b0ff'}}
-                        thumbColor={grains ? '#f5dd4b' : '#f4f3f4'}
-                        onValueChange={toggleSwitch(setGrain)}
-                        value={grains}
-                    />
-                    <Switch
-                        trackColor={{false: '#767577', true: '#81b0ff'}}
-                        thumbColor={dairy ? '#f5dd4b' : '#f4f3f4'}
-                        onValueChange={toggleSwitch(setDairy)}
-                        value={dairy}
-                    />
-                    <Switch
-                        trackColor={{false: '#767577', true: '#81b0ff'}}
-                        thumbColor={meats ? '#f5dd4b' : '#f4f3f4'}
-                        onValueChange={toggleSwitch(setMeat)}
-                        value={meats}
-                    />
+                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                        <Text>Fruit: {fruits? 'Y': 'N'}</Text>
+                        <Switch
+                            trackColor={{false: PHB_COLORS.WHITE, true: PHB_COLORS.WHITE}}
+                            thumbColor={fruits ? PHB_COLORS.BLUE : PHB_COLORS.LIGHTBLUE}
+                            onValueChange={toggleFruits}
+                            value={fruits}>
+                        </Switch>
+                    </View>
 
-                    <TextInput
-                        keyboardType="numeric"
-                        value={exercise}
-                        onChangeText={changeText(setExercise)}
-                        placeholder="Enter a number"
-                    />
+                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                        <Text>Vegetable: {vegetables? 'Y': 'N'}</Text>
+                        <Switch
+                            trackColor={{false: PHB_COLORS.WHITE, true: PHB_COLORS.WHITE}}
+                            thumbColor={vegetables ? PHB_COLORS.BLUE : PHB_COLORS.LIGHTBLUE}
+                            onValueChange={toggleVeg}
+                            value={vegetables}>
+                        </Switch>
+                    </View>
 
-                    <TextInput
+                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                        <Text>Grain: {grains? 'Y': 'N'}</Text>
+                        <Switch
+                            trackColor={{false: PHB_COLORS.WHITE, true: PHB_COLORS.WHITE}}
+                            thumbColor={grains ? PHB_COLORS.BLUE : PHB_COLORS.LIGHTBLUE}
+                            onValueChange={toggleGrain}
+                            value={grains}>
+                        </Switch>
+                    </View>
+
+                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                        <Text>Dairy: {dairy? 'Y': 'N'}</Text>
+                        <Switch
+                            trackColor={{false: PHB_COLORS.WHITE, true: PHB_COLORS.WHITE}}
+                            thumbColor={dairy ? PHB_COLORS.BLUE : PHB_COLORS.LIGHTBLUE}
+                            onValueChange={toggleDairy}
+                            value={dairy}>
+                        </Switch>
+                    </View>
+
+                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                        <Text>Protein: {meats? 'Y': 'N'}</Text>
+                        <Switch
+                            trackColor={{false: PHB_COLORS.WHITE, true: PHB_COLORS.WHITE}}
+                            thumbColor={meats ? PHB_COLORS.BLUE : PHB_COLORS.LIGHTBLUE}
+                            onValueChange={toggleMeat}
+                            value={meats}>
+                        </Switch>
+                    </View>
+
+                </ScrollView>
+            </InfoContainer>
+
+            <InfoContainer title="Exercise Logger">
+                <ScrollView  style={styles.scroll_view}>
+                    <Button title='Log Exercise' onPress={exercisePress}></Button>
+
+                    <Text>Steps today: {step_count}</Text>
+
+                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                        <TextInput
+                            keyboardType="default"
+                            value={exercise_type}
+                            onChangeText={changeType}
+                            placeholder="Enter workout type"
+                        />
+                        <TextInput
+                            keyboardType="numeric"
+                            value={exercise_duration}
+                            onChangeText={changeExercise}
+                            placeholder="Enter workout duration in minutes"
+                        />
+                    </View>
+                    
+                </ScrollView>
+            </InfoContainer>
+
+            <InfoContainer title="Wellness Logger">
+                <ScrollView  style={styles.scroll_view}>
+                    <Button title='Log Wellness' onPress={wellnessPress}></Button>
+                    {/* <TextInput
                         keyboardType="numeric"
                         value={sleep}
                         onChangeText={changeText(setSleep)}
                         placeholder="Enter a number"
                     />
-
                     <TextInput
                         keyboardType="numeric"
                         value={screen_time}
                         onChangeText={changeText(setScreen)}
                         placeholder="Enter a number"
                     /> */}
-                    
                 </ScrollView>
             </InfoContainer>
+
             </PHB_Body>
             <StatusBar style="auto" />
         </View>
     );
 }
-    
+
+
 const styles = StyleSheet.create({
     scroll_view:{
         height: '90%',
         width: '50%',
-        margin: '10%',
-    },
+        margin: '5%',
+    },      
 });
+
+
 
